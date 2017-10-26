58279674d173d62cdcd326c1697f08bcfd6fbb89
emmadema <emma.demango@gmail.com>
2017-10-25 18:50:18 -0600

    bonus done

---
+++ src/app/internal-defenses/internal-defenses.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TurretComponent } from './turret/turret.component';
+import { LaserCannonComponent } from './laser-cannon/laser-cannon.component';
+import { DeathSquareComponent } from './death-square/death-square.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+  	TurretComponent,
+  	LaserCannonComponent,
+  	DeathSquareComponent
+  ],
+  exports: [
+  	TurretComponent,
+  	LaserCannonComponent,
+  	DeathSquareComponent
+  ]
+})
+export class InternalDefensesModule { }
